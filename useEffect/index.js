6f5eae3816eb6e2d3c9d6716a7c430e21594f547
Justin <justinhwang92@gmail.com>
2021-06-18 13:57:18 -0400

    Code clean up

--- useEffect/index.js
+++ useEffect/index.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 
-
 const App = () => {
   const sayHello = () => console.log("hello");
-  //useEffect(sayHello);
+  // useEffect(sayHello);
   // same as above
   // useEffect(()=>{
   //   sayHello();
