9b6658a97768456ed95dabdecba5d2f7895564c1
Justin <justinhwang92@gmail.com>
2021-06-21 16:52:24 -0400

    Add some if statements and vars' name change in useConfirm

--- useConfirm/index.js
+++ useConfirm/index.js
@@ -1,15 +1,18 @@
 import React, { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
 
-const useConfirm = (message = "", callback, rejection) => {
-  if(typeof callback !== "function"){
+export const useConfirm = (message = "", onConfirm, onCancel) => {
+  if(!onConfirm && typeof onConfirm !== "function"){
+    return;
+  }
+  if(onCancel && typeof onCancel !== "function"){
     return;
   }
   const confirmAction = () => {
     if(confirm(message)){
-      callback();
+      onConfirm();
     } else {
-      rejection();
+      onCancel();
     }
   }
   return confirmAction;
