ae4ee9b4eb43c2ff131f5b0ff640e2b1fac04222
Justin <justinhwang92@gmail.com>
2021-06-24 17:11:17 -0400

    Add useNetwork hook

---
+++ useNetwork/index.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from "react";
+import ReactDOM from "react-dom";
+
+// useNetwork detects the navigator goes online or offline
+const useNetwork = (onChange) => {
+  const [status, setStatus] = useState(navigator.onLine || true);
+  const handleChange = () => {
+    if (onChange && typeof onChange === "function") {
+      onChange(navigator.onLine);
+    }
+    setStatus(navigator.onLine);
+  };
+
+  useEffect(() => {
+    window.addEventListener("online", handleChange);
+    window.addEventListener("offline", handleChange);
+
+    // when componentWillUnMount
+    () => {
+      window.removeEventListener("online", handleChange);
+      window.removeEventListener("offline", handleChange);
+    };
+  }, []);
+
+  return status;
+};
+
+const App = () => {
+  const handleNetworkChange = (online) => {
+    console.log(online ? "We just went online" : "We are offline");
+  };
+  const onLine = useNetwork(handleNetworkChange);
+  return (
+    <div className="App">
+      <h1>{onLine ? "Online" : "Offline"}</h1>
+    </div>
+  );
+};
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
